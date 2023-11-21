0089456406ca968ee6aa2c004e4dd3db9e1c75ed
SourSushi360 <jplopez365@hotmail.com>
2023-11-21 17:44:00 -0600

    added class par

--- Grafo.cpp
+++ Grafo.cpp
@@ -5,3 +5,14 @@
     Este archivo define las funciones declaradas en Grafo.hpp*/
 #include "Grafo.hpp"
 
+// Par
+Par::Par(int destino, double peso) {
+    *this->destino = destino;
+    *this->peso = peso;
+}
+Par::~Par() {
+    delete this->destino;
+    delete this->peso;
+}
+
+//Vertice
