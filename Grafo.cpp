3839333e02f4a91842fe55e1301dcebb88357a69
SourSushi360 <jplopez365@hotmail.com>
2023-11-21 17:51:45 -0600

    started vertice class with getters and setters

--- Grafo.cpp
+++ Grafo.cpp
@@ -2,7 +2,11 @@
     Autor: Jean Paul López Pándura
 
     Última modificación: 21/11/2023
-    Este archivo define las funciones declaradas en Grafo.hpp*/
+    Este archivo define las funciones declaradas en Grafo.hpp
+    Las clases que define son:
+    - Par
+    - Vertice
+    -Grafo */
 #include "Grafo.hpp"
 
 // Par
@@ -16,3 +20,33 @@ Par::~Par() {
 }
 
 //Vertice
+Vertice::Vertice() {
+    this->valor = new int;
+    this->estado = new bool;
+    this->conjunto = new bool;
+}
+Vertice::~Vertice() {
+    delete valor;
+    delete estado;
+    delete conjunto;
+}
+    // getters
+int Vertice::getValor() {
+    return *this->valor;
+}
+bool Vertice::getEstado() {
+    return *this->estado;
+}
+bool Vertice::getConjunto() {
+    return this->conjunto;
+}
+    // setters
+void Vertice::setValor(int valor) {
+    *this->valor = valor;
+}
+void Vertice::setEstado(bool estado) {
+    *this->estado = estado;
+}
+void Vertice::setConjunto(bool conjunto) {
+    *this->conjunto = conjunto;
+}
