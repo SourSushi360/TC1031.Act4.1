e3cd587cf15d8f18bf54fab42a81818d407a3ddb
SourSushi360 <jplopez365@hotmail.com>
2023-11-25 20:47:38 -0600

    updated bfs and dfs

--- Grafo.cpp
+++ Grafo.cpp
@@ -9,6 +9,8 @@
     -Grafo */
 #include "Grafo.hpp"
 #include <iostream>
+#include <stack>
+#include <queue>
 
 // Par
 Par::Par(int destino, double peso) {
@@ -83,24 +85,79 @@ void Grafo::loadGraph(int numVertices, int numAristas, double **matrizAdjacencia
     this->vertices = new Vertice[numVertices];
     this->matriz = new double*[numVertices];
     for (int i = 0; i < numVertices; ++i) {
+        this->vertices[i].setValor(i);
         this->matriz[i] = new double[numVertices];
         for (int j = 0; j < numVertices; ++j) {
             this->matriz[i][j] = matrizAdjacencia[i][j];
+            if (matrizAdjacencia[i][j] != std::numeric_limits<double>::infinity()) {
+              this->vertices[i].agregaArista(j, matrizAdjacencia[i][j]);
+            }
         }
     }
 }
         //Realiza búsqueda en profundidad e imprime 
         //los id de los vértices recorridos separados por comas
 void Grafo::dfs(int verticeInicial) {
+    for (int i = 0;i < *this->numVertices;i++) {
+        this->vertices[i].setEstado(false);
+    }
+
+    std::stack<Vertice> pila;
+    pila.push(vertices[verticeInicial]);
+    Vertice verticeActual;
+    // Iterar hasta que la pila esté vacía
+    while (!pila.empty()) {
+        verticeActual = pila.top();
+        pila.pop();
+        // Si el vértice no ha sido visitado marcar el vértice como visitado e imprimir su ID
+        if (!verticeActual.getEstado()) {
+            verticeActual.setEstado(true);
+            std::cout << verticeActual.getValor() << ", ";
+
+            // Agregar los vértices adyacentes no visitados a la pila
+            for (int i = 0;i < *this->numVertices;i++) {
+                if (this->matriz[verticeActual.getValor()][i] != std::numeric_limits<double>::infinity()
+                && !this->matriz[verticeActual.getEstado()][i]) {
+                    pila.push(this->vertices[i]);
+                }
+            }
+        }
+    }
 }
         //Realiza búsqueda en amplitud e imprime
         //los id de los vértices recorridos separados por comas
 void Grafo::bfs(int verticeInicial) {
+    for (int i = 0;i < *this->numVertices;i++) {
+        this->vertices[i].setEstado(false);
+    }
+
+    // Utilizar una cola para la búsqueda en amplitud
+    std::queue<Vertice> cola;
+    cola.push(this->vertices[verticeInicial]);
+    Vertice verticeActual;
+    // Iterar hasta que la pila esté vacía
+    while (!cola.empty()) {
+        verticeActual = cola.front();
+        cola.pop();
+        // Si el vértice no ha sido visitado marcar el vértice como visitado e imprimir su ID
+        if (!verticeActual.getEstado()) {
+            verticeActual.setEstado(true);
+            std::cout << verticeActual.getValor() << ", ";
+
+            // Agregar los vértices adyacentes no visitados a la pila
+            for (int i = 0;i < *this->numVertices;i++) {
+                if (this->matriz[verticeActual.getValor()][i] != std::numeric_limits<double>::infinity()
+                && !this->matriz[verticeActual.getEstado()][i]) {
+                    cola.push(this->vertices[i]);
+                }
+            }
+        }
+    }
 }
         //Regresa true si el Grafo Dirigido Acíclico (DAG) es un árbol
         //Nota, el grafo proporcionado deberá ser un DAG.
 bool Grafo::isTree() {
-
+    
 }
         //Regresa true si el DAG proporcionado es un grafo Bipartita
 bool Grafo::isBipartiteGraph(int numVertices, int numAristas, double **matrizAdjacencia) {
